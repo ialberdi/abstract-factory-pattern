2336f514c9a864d748094a9d8cf76b0591513c08
Iker Alberdi <it@recambiosviaweb.com>
2018-10-25 23:32:56 +0200

    Initial commit

---
+++ src/AbstractFactory/ServicioFactory.java
@@ -0,0 +1,5 @@
+package AbstractFactory;
+
+public interface ServicioFactory {
+    ServicioInformatico crearServicio();
+}
---
+++ src/AbstractFactory/ServicioInformatico.java
@@ -0,0 +1,7 @@
+package AbstractFactory;
+
+public interface ServicioInformatico {
+    void asignarTrabajo();
+    void indicarFechaEntrega();
+    void informarSobrePago();
+}
